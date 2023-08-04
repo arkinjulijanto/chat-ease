a0e1340498f7c7b0443b81c3bcd6bb73118e1844
Arkin Eliezer Julijanto <arkin.julijanto@shopee.com>
2023-08-04 16:36:50 +0700

    feat: EC-9/register route

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
+import Chat from './pages/Chat';
 
 function App() {
   return (
     <Routes>
       <Route path="login" element={<Login />}></Route>
+      <Route path="" element={<Chat />}></Route>
     </Routes>
   );
 }
