5db7630315df54ee3642f815b474cedbcbf2fc80
Arkin Eliezer Julijanto <arkin.julijanto@shopee.com>
2023-08-04 16:51:15 +0700

    feat: EC-9/add base box

--- src/pages/Chat/index.tsx
+++ src/pages/Chat/index.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
 function Chat() {
-  return <div>Test</div>;
+  return (
+    <div className="h-screen flex px-0 py-0 md:px-80 md:py-10">
+      <div className="w-full h-full max-h-lg m-auto border border-gray-200 rounded-none md:rounded-lg shadow sm:p-6">
+        <h5 className="mb-2 text-base font-semibold text-gray-900 md:text-xl dark:text-white">
+          Test
+        </h5>
+      </div>
+    </div>
+  );
 }
 
 export default Chat;
