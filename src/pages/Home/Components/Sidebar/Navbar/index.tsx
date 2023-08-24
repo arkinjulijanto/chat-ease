334b52a64483a1131c85b7aa4a773d9f481ce053
Arkin Eliezer Julijanto <arkin.julijanto@shopee.com>
2023-08-24 12:37:35 +0700

    feat: EC-9/add dummy profile pict

--- src/pages/Home/Components/Sidebar/Navbar/index.tsx
+++ src/pages/Home/Components/Sidebar/Navbar/index.tsx
@@ -3,7 +3,11 @@ import React from 'react';
 function Navbar() {
   return (
     <div className="flex items-center p-4 h-[50px] justify-between text-gray-700 dark:text-gray-200">
-      <img src="" alt="profile" className="h-[24px] w-[24px] rounded object-cover" />
+      <img
+        src="https://images.pexels.com/photos/4133339/pexels-photo-4133339.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load"
+        alt="profile"
+        className="h-[40px] w-[40px] rounded object-cover"
+      />
       <div className="flex gap-2">
         <button className="w-[34px] p-1 border rounded border-slate-500 bg-slate-200 dark:bg-slate-600">
           <svg
