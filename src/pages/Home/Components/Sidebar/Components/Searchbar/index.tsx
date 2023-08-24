08e47aa644d85ba4cc5dc9b553fdaf255fce2e81
Arkin Eliezer Julijanto <arkin.julijanto@shopee.com>
2023-08-24 12:59:25 +0700

    feat: EC-9/add search bar

---
+++ src/pages/Home/Components/Sidebar/Components/Searchbar/index.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+function Searchbar() {
+  return (
+    <form className="mt-2 mx-4">
+      <label
+        htmlFor="default-search"
+        className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+      >
+        Search
+      </label>
+      <div className="relative">
+        <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+          <svg
+            className="w-4 h-4 text-gray-500 dark:text-gray-400"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 20 20"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+            />
+          </svg>
+        </div>
+        <input
+          type="text"
+          id="default-search"
+          className="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-md bg-gray-50 focus:outline-none focus:ring-gray-500 focus:border-gray-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-gray-500 dark:focus:border-gray-500"
+        />
+      </div>
+    </form>
+  );
+}
+
+export default Searchbar;
--- src/pages/Home/Components/Sidebar/index.tsx
+++ src/pages/Home/Components/Sidebar/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from './Components/Navbar';
+import Searchbar from './Components/Searchbar';
 
 function Sidebar() {
   return (
     <div className="basis-1/3 bg-slate-200 dark:bg-slate-700">
       <Navbar />
+      <Searchbar />
     </div>
   );
 }
